43ef47da195230c0acb1420897201fe0344dd553
dpdonohue <dd@daviddonohue.com>
2017-11-28 06:18:43 -0500

    trying to get this to run on Azure

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const CureServer = require('./js/CureServer.js');
 module.exports = CureServer;
 
-
-
 // Setup Restify Server
 var server = new CureServer((session, postObject) => {
     console.log("index.js: save postObject:", postObject);
 });
 
+console.log("biologio-curebot: created new CureServer");
